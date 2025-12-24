7ec4231518216bcd781a7659d9bf479a341de0ce
Sfer7 <forever_kaka@naver.com>
2025-12-24 17:51:47 +0900

    feat: 초기세팅

---
+++ src/main/java/stack/moaticket/MoaticketApplication.java
@@ -0,0 +1,13 @@
+package stack.moaticket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoaticketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoaticketApplication.class, args);
+    }
+
+}

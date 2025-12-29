0382cf904a1857acca9705ddd9f7b18b4c76ee4d
김민경 <kimmg0708@gmail.com>
2025-12-29 13:30:19 +0900

    feat: exception 수정

--- src/main/java/stack/moaticket/system/exception/ErrorCode.java
+++
@@ -1,9 +0,0 @@
-package stack.moaticket.system.exception;
-
-import org.springframework.http.HttpStatus;
-
-public interface ErrorCode {
-    HttpStatus getStatus();
-    String getCode();
-    String getMessage();
-}

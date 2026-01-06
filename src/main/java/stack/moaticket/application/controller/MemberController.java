2f4380ee49e31ead48625e1953bb90399470a01b
권순일 <rnjstnsdlf09@gmail.com>
2026-01-06 12:51:06 +0900

    feat: fix MemberInfoService로 이름 변경, 불필요한 코드 제거, 변수명 변경

--- src/main/java/stack/moaticket/application/controller/MemberController.java
+++ src/main/java/stack/moaticket/application/controller/MemberController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.RestController;
 import stack.moaticket.application.dto.GetMemberDto;
 import stack.moaticket.application.service.MemberInfoService;
 import stack.moaticket.domain.member.entity.Member;
+import stack.moaticket.system.exception.MoaException;
+import stack.moaticket.system.exception.MoaExceptionType;
 
 @RestController
 @RequiredArgsConstructor
@@ -15,10 +17,13 @@ public class MemberController {
 
     private final MemberInfoService memberInfoService;
 
-    @GetMapping("/member")
+    @GetMapping("/api/members/me")
     public ResponseEntity<GetMemberDto.Response> getMember(
             @AuthenticationPrincipal Member member
     ) {
+        if (member == null) {
+            throw new MoaException(MoaExceptionType.NOT_FOUND);
+        }
         return ResponseEntity.ok(memberInfoService.getMember(member));
     }
 }

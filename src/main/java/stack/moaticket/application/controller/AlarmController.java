de8e6a50ead32e16517e8ec4d6418c3b6631da4b
Sfer7 <forever_kaka@naver.com>
2026-01-18 20:56:58 +0900

    fix: AlarmController단의 불필요한 코드 제거

--- src/main/java/stack/moaticket/application/controller/AlarmController.java
+++ src/main/java/stack/moaticket/application/controller/AlarmController.java
@@ -1,11 +1,9 @@
 package stack.moaticket.application.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
-import stack.moaticket.application.dto.SeatAlarmReserve;
 import stack.moaticket.application.service.AlarmService;
 import stack.moaticket.domain.member.entity.Member;
 
@@ -18,7 +16,6 @@ public class AlarmController {
     @GetMapping(value = "/sub", produces = "text/event-stream")
     public SseEmitter subscribe(
             @AuthenticationPrincipal Member member) {
-        System.out.println("멤버 아이디: " + member.getId());
         return alarmService.subscribe(member);
     }
 }

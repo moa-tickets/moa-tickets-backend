d38d3ca450a0db580744bca3de48b80ccf59b8bb
Sfer7 <forever_kaka@naver.com>
2026-01-22 20:28:44 +0900

    fix: PaymentRepositoryQueryDsl 복구

--- src/main/java/stack/moaticket/domain/payment/repository/PaymentRepositoryQueryDsl.java
+++ src/main/java/stack/moaticket/domain/payment/repository/PaymentRepositoryQueryDsl.java
@@ -5,7 +5,6 @@ import jakarta.persistence.LockModeType;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 import stack.moaticket.domain.payment.entity.Payment;
-import stack.moaticket.domain.payment.type.PaymentState;
 
 import static stack.moaticket.domain.payment.entity.QPayment.payment;
 
@@ -18,7 +17,7 @@ public class PaymentRepositoryQueryDsl {
     public Payment findByOrderIdForUpdate(String orderId){
         return queryFactory
                 .selectFrom(payment)
-                .where(payment.orderId.eq(orderId).and(payment.state.eq(PaymentState.READY)))
+                .where(payment.orderId.eq(orderId))
                 .setLockMode(LockModeType.PESSIMISTIC_WRITE)
                 .fetchOne();
     }

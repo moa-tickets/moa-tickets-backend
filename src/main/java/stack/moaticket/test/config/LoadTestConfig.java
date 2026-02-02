6ef1e184cf4c81a9190f80b8d29d661dcf5232af
Sfer7 <forever_kaka@naver.com>
2026-02-02 13:08:05 +0900

    fix: loadtest property 명시적 추가

--- src/main/java/stack/moaticket/test/config/LoadTestConfig.java
+++ src/main/java/stack/moaticket/test/config/LoadTestConfig.java
@@ -1,12 +1,14 @@
 package stack.moaticket.test.config;
 
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import stack.moaticket.system.alarm.sse.service.SseSubscribeService;
 import stack.moaticket.test.controller.AlarmTestController;
 import stack.moaticket.test.service.AlarmTestService;
 
+@EnableConfigurationProperties(LoadTestProperties.class)
 @ConditionalOnProperty(
         value = "app.server.test.load-test.enabled",
         havingValue = "true",

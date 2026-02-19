7d30f539bdc38c221777abdf0df2b823542d0a7f
Sfer7 <forever_kaka@naver.com>
2026-02-19 18:05:24 +0900

    fix: session start alarm transaction isolation 변경

--- src/main/java/stack/moaticket/application/facade/ConcertInformFacade.java
+++ src/main/java/stack/moaticket/application/facade/ConcertInformFacade.java
@@ -2,6 +2,7 @@ package stack.moaticket.application.facade;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Isolation;
 import org.springframework.transaction.annotation.Transactional;
 import stack.moaticket.application.component.gauge.SessionStartGaugeManager;
 import stack.moaticket.domain.session_start_alarm.dto.SessionStartAlarmMetaDto;
@@ -16,7 +17,7 @@ public class ConcertInformFacade {
     private final SessionStartAlarmService sessionStartAlarmService;
     private final SessionStartGaugeManager manager;
 
-    @Transactional
+    @Transactional(isolation = Isolation.READ_COMMITTED)
     public List<Long> passAndProcess(LocalDateTime now, Long batchSize) {
         List<Long> idList = sessionStartAlarmService.getPendingSessionAlarmIdList(now, batchSize);
         if(idList.isEmpty()) return List.of();

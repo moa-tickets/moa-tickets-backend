761a2e43c4102d80a6b97dfbf7512ac8918c68de
Sfer7 <forever_kaka@naver.com>
2026-02-21 21:15:37 +0900

    feat: pel drop 기능 추가

--- src/main/java/stack/moaticket/application/component/gauge/TicketReleaseMarks.java
+++ src/main/java/stack/moaticket/application/component/gauge/TicketReleaseMarks.java
@@ -8,9 +8,11 @@ public final class TicketReleaseMarks  {
     private boolean unlocked = false;
     private boolean done = false;
     private boolean acked = false;
+    private boolean dropped = false;
 
     public void markLocked() { locked = true; }
     public void markUnlocked() { unlocked = true; }
     public void markDone() { done = true; }
     public void markAcked() { acked = true; }
+    public void markDropped() { dropped = true; }
 }
